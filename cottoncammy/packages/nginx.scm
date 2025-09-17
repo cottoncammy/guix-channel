0cb4c8ba63cf07293e52911e6337531f2346116b
cottoncammy <cam@cottoncammy.xyz>
2025-09-17 16:29:00 -0400

    Add deps to native deps so it will build

--- cottoncammy/packages/nginx.scm
+++ cottoncammy/packages/nginx.scm
@@ -1,5 +1,9 @@
 (define-module (cottoncammy packages nginx)
   #:use-module ((gnu packages web) #:prefix web:)
+  #:use-module (gnu packages xml)
+  #:use-module (gnu packages pcre)
+  #:use-module (gnu packages tls)
+  #:use-module (gnu packages compression)
   #:use-module (guix packages)
   #:use-module (guix gexp)
   #:use-module (guix utils))
@@ -7,11 +11,12 @@
 (define-public nginx
   (package
     (inherit web:nginx)
+    (native-inputs (list libxml2 libxslt pcre openssl zlib))
     (arguments
       (substitute-keyword-arguments (package-arguments web:nginx)
         ((#:phases phases)
           #~(modify-phases #$phases
-              (add-before 'configure 'test-patch
+              (add-before 'configure 'patch-ngx-test-cc
                 (lambda _
                   (for-each (lambda (file)
                               (substitute* file
