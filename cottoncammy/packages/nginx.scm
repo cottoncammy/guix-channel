f5f6f5c943acdf98d24859a09c3a27aafb19bcca
cottoncammy <cam@cottoncammy.xyz>
2025-09-17 15:15:43 -0400

    add temporary nginx packaige variant

--- cottoncammy/packages/nginx.scm
+++ cottoncammy/packages/nginx.scm
@@ -2,44 +2,24 @@
   #:use-module ((gnu packages web) #:prefix web:)
   #:use-module (guix packages)
   #:use-module (guix gexp)
-  #:use-module (guix utils)
-  #:use-module (ice-9 match))
+  #:use-module (guix utils))
 
 (define-public nginx
   (package
     (inherit web:nginx)
     (arguments
       (substitute-keyword-arguments (package-arguments web:nginx)
-        ((#:configure-flags _)
-         #~(list "--with-http_ssl_module"
-                 "--with-http_v2_module"
-                 "--with-http_xslt_module"
-                 "--with-http_gzip_static_module"
-                 "--with-http_gunzip_module"
-                 "--with-http_addition_module"
-                 "--with-http_sub_module"
-                 "--with-pcre-jit"
-                 "--with-debug"
-                 "--with-compat"
-                 "--with-stream"
-                 "--with-stream_ssl_module"
-                 "--with-http_stub_status_module"
-                 ;; Even when not cross-building, we pass the
-                 ;; --crossbuild option to avoid customizing for the
-                 ;; kernel version on the build machine.
-                 #$(let ((system "Linux")  ; uname -s
-                         (release "3.2.0") ; uname -r
-                         ;; uname -m
-                         (machine (match (or (%current-target-system)
-                                             (%current-system))
-                                    ("x86_64-linux"   "x86_64")
-                                    ("i686-linux"     "i686")
-                                    ("mips64el-linux" "mips64")
-                                    ("aarch64-linux"  "aarch64")
-                                    ;; Prevent errors when querying
-                                    ;; this package on unsupported
-                                    ;; platforms, e.g. when running
-                                    ;; "guix package --search="
-                                    (_                "UNSUPPORTED"))))
-                     (string-append "--crossbuild="
-                                    system ":" release ":" machine))))))))
+        ((#:phases phases)
+          #~(modify-phases #$phases
+              (add-before 'configure 'test-patch
+                (lambda _
+                  (for-each (lambda (file)
+                              (substitute* file
+                                (("^(\\s*ngx_test=\")(\\$CC)(.*)$" all begin cc end)
+                                 (string-append begin (which "gcc") end))))
+                            '("auto/endianness"
+                              "auto/feature"
+                              "auto/include"
+                              "auto/types/sizeof"
+                              "auto/types/typedef"
+                              "auto/types/uintptr_t"))))))))))
