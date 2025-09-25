a9bb8d5f21cdf53f25c8398bef3ddae11f3b3e9e
cottoncammy <cam@cottoncammy.xyz>
2025-09-25 15:44:28 -0400

    .

--- cottoncammy/packages/dropbear.scm
+++ cottoncammy/packages/dropbear.scm
@@ -32,4 +32,4 @@
                   (lambda (package)
                     (not (string=? (car package) "libtomcrypt")))
                   (package-inputs base))
-                `(("libtomcrypt" . libtomcrypt)))))))
+                (cons ("libtomcrypt" libtomcrypt)))))))
