4f278f6dfb20601180868dd0e2f9302fd506beab
cottoncammy <cam@cottoncammy.xyz>
2025-09-26 14:17:02 -0400

    fix

--- cottoncammy/packages/dropbear.scm
+++ cottoncammy/packages/dropbear.scm
@@ -23,6 +23,29 @@
           (sha256
             (base32
               "00yx4nl8ywji5lk5m92927ab68gjv17ihw1phsi71l4vm4jvpc54"))))
+      (arguments
+        (substitute-keyword-arguments (package-arguments base)
+          ((#:phases phases)
+            #~(modify-phases #$phases
+                (replace 'prepare-build
+                  (lambda _
+                    ;; We want the shared library by default so force it to be the
+                    ;; default makefile target.
+                    (delete-file "makefile")
+                    (symlink "makefile.shared" "makefile")
+                    #t))
+                (replace 'install-static-library
+                  (lambda* (#:key outputs make-flags #:allow-other-keys)
+                    (apply invoke "make" "-f" "makefile.unix" "install" make-flags)
+                    (let ((out (assoc-ref outputs "out"))
+                          (static (assoc-ref outputs "static")))
+                      (mkdir-p (string-append static "/lib"))
+                      (mkdir-p (string-append static "/include"))
+                      (rename-file (string-append out "/lib/libtomcrypt.a")
+                                   (string-append static "/lib/libtomcrypt.a"))
+                      (copy-recursively (string-append out "/include")
+                                        (string-append static "/include"))
+                      #t)))))))
       (native-inputs '()))))
 
 (define-public dropbear
